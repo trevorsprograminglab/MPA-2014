1688e9805c1f8457ffe8e88f3fcbc8168c70641b
Trevor Eady <teady5088@gmail.com>
2014-06-16 09:43:44 -0400

    Main Commit
    First Commit

---
+++ ver 1.0/src/pkgfinal/assignment/FinalAssignment.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkgfinal.assignment;
+
+/**
+ *
+ * @author Trevor Eady
+ */
+public class FinalAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main maingui = new Main();
+        PointsGui pointsgui = new PointsGui();
+        maingui.setVisible(true);
+        //pointsgui.setBounds(500, 0, 121, 200);
+       // pointsgui.setVisible(true);
+        
+    }
+    
+}
